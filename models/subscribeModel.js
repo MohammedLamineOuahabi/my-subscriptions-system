3a91b9b346c66311605dc95b410a25967e370737
amine <amine@adress.com>
2020-11-27 18:57:59 +0100

    add subscribe status

--- models/subscribeModel.js
+++ models/subscribeModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const subscribeSchema = mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
+    status: { type: Number, required: true, default: 0 },
     list: { type: String, required: true }
   },
   { timestamps: true }
