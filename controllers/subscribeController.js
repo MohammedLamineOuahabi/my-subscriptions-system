dea86ad6cf143f6a0dc0f60322196b83a83ff3a1
Your Name <you@example.com>
2022-06-28 16:14:08 +0100

    change version,add log

--- controllers/subscribeController.js
+++ controllers/subscribeController.js
@@ -30,7 +30,7 @@ const addSubscribe = asyncHandler(async (req, res) => {
     res.status(201).json({ success: true });
   } catch (error) {
     //throw new Error(error);
-    res.status(500).json({ success: false });
+    res.status(500).json({ success: false, message: error.message });
   }
 });
 
